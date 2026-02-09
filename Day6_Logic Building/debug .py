3130b96e3cbb62def32c97205ef80acfd1177cba
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-09 18:15:48 +0530

    Create debug .py

---
+++ Day6_Logic Building/debug .py
@@ -0,0 +1,24 @@
+1.wrong program
+
+x = 10
+y = 5
+print("Sum:", x + )
+
+
+1.debug program
+
+x = 10
+y = 5
+print("Sum:", x + y)
+
+
+2.wrong program
+
+for i in range(5)
+    print(i)
+
+2.debug program
+
+for i in range(5):
+    print(i)
+
